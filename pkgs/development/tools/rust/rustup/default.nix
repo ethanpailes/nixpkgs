e0106a24cb1a56e93bc7ee61cbe76fb6eb10b234
Ethan Pailes <ethanpailes@gmail.com>
2016-05-02 20:58:51 -0400

    more ineffectual flaling to get try to install rustup.

--- pkgs/development/tools/rust/rustup/default.nix
+++ pkgs/development/tools/rust/rustup/default.nix
@@ -1,12 +1,35 @@
-{ stdenv, openssl, fetchFromGitHub, rustPlatform }:
+{ stdenv, tree, openssl, fetchFromGitHub, rustUnstable, cargo }:
 
 let
   version = "0.1.6";
 in
 
-with rustPlatform;
+with rustUnstable;
 
-buildRustPackage rec {
+# stdenv.mkDerivation rec {
+    # name = "rustup-${version}";
+# 
+    # src = fetchFromGitHub {
+        # owner = "rust-lang-nursery";
+        # repo  = "rustup.rs";
+        # rev   = "${version}";
+        # sha256 = "0hldsb1ah0khr9fgkpy0158yjkvzx37nrcplcy0a4wyj3fgcjk2m";
+    # };
+# 
+    # buildInputs = [ openssl cargo rustUnstable.rustc rustUnstable.rustRegistry ];
+# 
+    # buildPhase = ''
+        # cat << EOF > deps/config
+# [registry]
+# index = \"file:///dev/null\"
+# EOF
+        # echo "running: cargo build --release"
+        # cargo build --verbose --release
+    # '';
+# }
+
+
+buildRustPackage {
   name = "rustup-${version}";
   src = fetchFromGitHub {
     owner = "rust-lang-nursery";
@@ -18,12 +41,37 @@ buildRustPackage rec {
   depsSha256 = "0wy6czcmgsc9n95zpaiihy1pb7zhidq569y57ka5dxpmkbwjx2kp";
   # depsSha256 = "1r2fxirkc0y6g7aas65n3yg1f2lf3kypnjr2v20p5np2lvla6djj";
 
-  buildInputs = [ openssl ];
+  buildInputs = [ tree openssl ];#rustUnstable.rustc rustUnstable.rustRegistry ];
 
-  installPhase = ''
-    mkdir -p $out/bin
-    cp -p target/release/rustup $out/bin/
+  # unpackPhase = ''
+    # cat env-vars
+  # '';
+
+  preUnpack = ''
+    mkdir -p deps/registry/index/-2304f9dd481834b8
+    echo "source files are:"
+    tree /nix/store/s14nrr24mjv7g7326v60w2m1hq8v9fck-rustup.rs-0.1.6-src
   '';
+
+  postUnpack = ''
+    echo "Made it through unpack."
+  '';
+
+  buildPhase = ''
+    echo "build"
+    tree .
+  '';
+
+  # configurePhase = ''
+  #   echo $(pwd)
+  #   tree .
+  # '';
+
+  # The default install phase should work.
+  # installPhase = ''
+  #   mkdir -p $out/bin
+  #   cp -p target/release/rustup $out/bin/
+  # '';
 
   meta = with stdenv.lib; {
     description = "A toolchain multiplexer for the rust programming language.";
